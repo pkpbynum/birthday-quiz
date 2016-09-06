1eab3187e3850c69c1502400e67d8e36b8ec28a7
peterbynum <peterbynum@hanovernorwichschools.org>
2016-09-05 21:52:23 -0400

    Updated from Brython Server: 9/5/2016 9:52:22 PM

--- birthday.py
+++ birthday.py
@@ -42,8 +42,53 @@ month = input(("Hi {0}, what was the name of the month you were born in? ").form
 year = input(("And what year were you born in, {0}? ").format(name))
 day = input("And the day? ")
 
-If month is "October" and day == "31":
+if month == "October" and day == "31":
     print("You were born on Halloween!")
+elif month == todaymonth and day == str(todaydate):
+    print("Happy birthday!")
+else:
+    if month == "December" or month == "January" or month == "February":
+        season = "winter"
+        if int(year) >= 2000 and int(year) <= 2016:
+            age = "two thousands"
+        elif int(year) >= 1990 and int(year) < 2000:
+            age = "nineties"
+        elif int(year) >= 1980 and int(year) <1990:
+            age = "eighties"
+        else:
+            age = "Stone Age"
+    elif month == "March" or month == "April" or month == "May":
+        season = "spring"
+        if int(year) >= 2000 and int(year) <= 2016:
+            age = "two thousands"
+        elif int(year) >= 1990 and int(year) < 2000:
+            age = "nineties"
+        elif int(year) >= 1980 and int(year) <1990:
+            age = "eighties"
+        else:
+            age = "Stone Age"
+    elif month == "June" or month == "July" or month == "August":
+        season = "summer"
+        if int(year) >= 2000 and int(year) <= 2016:
+            age = "two thousands"
+        elif int(year) >= 1990 and int(year) < 2000:
+            age = "nineties"
+        elif int(year) >= 1980 and int(year) <1990:
+            age = "eighties"
+        else:
+            age = "Stone Age"
+    else:
+        season = "fall"
+        if int(year) >= 2000 and int(year) <= 2016:
+            age = "two thousands"
+        elif int(year) >= 1990 and int(year) < 2000:
+            age = "nineties"
+        elif int(year) >= 1980 and int(year) <1990:
+            age = "eighties"
+        else:
+            age = "Stone Age"
+    print(("{0}, you are a {1} baby of the {2}.").format(name, season, age))
+
 
 
 
